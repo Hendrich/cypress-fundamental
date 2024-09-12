6a6cfafa47013b021b388cd4452376ef983b608a
hendrich <hendri.christianto24@gmail.com>
2024-09-12 16:34:14 +0700

    penambahan beberapa file di branch featur_b

---
+++ variable.js
@@ -0,0 +1,24 @@
+
+// use var
+var x1 = 100;
+var y1 = 200;
+var z1 = x1 + y1;
+console.log("this value of z if using var variable is : " + z1);
+
+// use let
+let x2 = 100;
+let y2 = 200;
+let z2 = x2 + y2;
+console.log("this value of z if using let variable is : " + z2);
+
+// use nothing
+x3 = 100;
+y3 = 200;
+z3 = x3 + y3;
+console.log("this value of z if using nothing variable is : " + z3);
+
+// use const
+const x4 = 100;
+const y4 = 200;
+const z4 = x4 + y4;
+console.log("this value of z if using const variable is : " + z4);
