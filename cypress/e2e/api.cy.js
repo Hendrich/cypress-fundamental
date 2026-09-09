d038a5550a61e330d17ee4febe8b3c5451783894
Hendrich <hendri.christianto24@gmail.com>
2026-09-09 16:13:29 +0700

    fix:perbaikan di file api

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -1,15 +1,32 @@
 describe("API", () => {
+
+  let token = "";
+  it("Hit api method POST", () => {
+    cy.request({
+      method: "POST",
+      url: "https://api-script-labs.hendri.me/api/auth/login",
+      headers: {},
+      body: {
+        "email": "standard_user@example.com",
+        "password": "script_sauce"
+      },
+    }).then((response) => {
+      cy.log(JSON.stringify(response.body));
+      expect(response.status).to.eq(200);
+      expect(response.body.data.token).to.be.a("string");
+      token = response.body.data.token;
+    });
+  });
+
   it("Hit api method GET", () => {
     cy.request({
       method: "GET",
-      url: "https://gorest.co.in/public/v2/users",
-      headers: {},
-      body: {},
+      url: "https://api-script-labs.hendri.me/api/labs",
+      headers: {Authorization: `Bearer ${token}`},
     }).then((response) => {
-      cy.log(response);
+      cy.log(JSON.stringify(response.body.data));
       expect(response.status).to.eq(200);
-      expect(response.body[0].gender).to.equal("female");
-      expect(response.body[0].name).to.is.not.null;
+      expect(response.body.data).to.be.an("array");
     });
   });
 });
